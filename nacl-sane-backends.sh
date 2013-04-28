0193772b114bde24d1fe1f2b7e1b6f58e045206c
Andrew de los Reyes <adlr@chromium.org>
2013-04-28 16:18:35 -0700

    initial revision

---
+++ nacl-sane-backends.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+# nacl-zlib-1.2.3.sh
+#
+# usage:  nacl-zlib-1.2.3.sh
+#
+# this script downloads, patches, and builds zlib for Native Client 
+#
+
+NACLPORTS_CFLAGS="-O0"
+
+source pkg_info
+source ../../build_tools/common.sh
+
+CustomExtractStep() {
+  Banner "Copying source from git repo"
+  ChangeDir ${NACL_PACKAGES_REPOSITORY}
+  Remove ${PACKAGE_DIR}
+  cp -Rp /home/adlr/sane-backends ${PACKAGE_DIR}
+}
+
+CustomConfigureStep() {
+  CXXFLAGS="-O0 -g" CFLAGS="-I${NACL_SDK_ROOT}/include" DefaultConfigureStep --disable-ipv6 --disable-preload --enable-latex=no --enable-avahi=no --enable-static=no --enable-shared=yes
+  #Banner "Configuring ${PACKAGE_NAME}"
+  #ChangeDir ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_NAME}
+  ## TODO: side-by-side install
+  #CC=${NACLCC} AR="${NACLAR} -r" RANLIB=${NACLRANLIB} CFLAGS="-Dunlink=puts" ./configure\
+  #   --prefix=${NACLPORTS_PREFIX} --disable-ipv6 --disable-preload --disable-latex --host=${NACL_CROSS_PREFIX}
+  echo "#define HAVE_SIGPROCMASK 1" >> include/sane/config.h
+}
+
+
+CustomBuildStep() {
+  make clean
+  cd backend && make -j${OS_JOBS} .libs/libsane.so.1.0.24
+  #DefaultBuildStep
+}
+
+CustomPackageInstall() {
+  DefaultPreInstallStep
+  #DefaultDownloadStep
+  #DefaultExtractStep
+  CustomExtractStep
+  # zlib doesn't need patching, so no patch step
+  CustomConfigureStep
+  #CustomBuildStep
+  DefaultBuildStep
+
+  echo ${NACLCXX} -g -O0 -o adlrsane.nexe ../nacl_main.cc -I${NACL_SDK_ROOT}/include `find . -type f | grep \\\\.a\\$  | xargs -n 1 dirname | uniq | xargs -I X echo -LX` `find . -type f | grep \\\\.a\\$ | xargs -n 1 basename | sed 's/^lib\\(.*\\)\\.a/-l\\1/g'` -lppapi_cpp -lppapi
+  ${NACLCXX} -g -O0 -o adlrsane.nexe ../nacl_main.cc -I${NACL_SDK_ROOT}/include `find . -type f | grep \\\\.a\\$  | xargs -n 1 dirname | uniq | xargs -I X echo -LX` `find . -type f | grep \\\\.a\\$ | xargs -n 1 basename | sed 's/^lib\\(.*\\)\\.a/-l\\1/g'` -lppapi_cpp -lppapi
+
+  DefaultInstallStep
+  DefaultCleanUpStep
+}
+
+
+CustomPackageInstall
+#DefaultPackageInstall
+exit 0
